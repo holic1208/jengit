78156ce1b0c90e95d68d0df08b247304bed6febf
suuu <holic1208@daum.net>
2021-11-18 16:43:34 +0900

    step3

--- http.sh
+++ http.sh
@@ -3,5 +3,5 @@ rm -rf /var/www/html/index.html
 yum remove -y httpd
 yum install -y httpd
 chmod 777 /var/www/html
-echo "Jenkins-github-colla" >> var/www/html/index.html
+echo "Jenkins-github!!!-colla" >> var/www/html/index.html
 systemctl start httpd
