d1ebd829baa41a583231586b3e96a99396694483
suuu <holic1208@daum.net>
2021-11-18 15:40:51 +0900

    step2

--- dock.sh
+++ dock.sh
@@ -2,3 +2,5 @@
 amazon-linux-extras install -y docker
 systemctl start docker
 docker pull jenkins/jenkins:lts
+sudo docker run -itd -p 60800:8080 -v /test:/var/jenkins_home --name j1 jenkins/jenkins:lts
+sudo docker exec j1 cat  /var/jenkins_home/secrets/initialAdminPassword
